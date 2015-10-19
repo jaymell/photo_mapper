e67f5f7c1fe3b13f65d46403fbf55db3a3d209ec
james <jamesmlucas@gmail.com>
2015-10-19 00:44:29 -0500

    Work has begun in earnest on import_data script

--- import_data.py
+++ import_data.py
@@ -10,74 +10,72 @@ import json
 from pymongo import MongoClient
 import re
 
-import sys
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-DB_NAME = 'photo_mapper'
-
-
-#### let's not make this global, eh?
-collection = connection[DB_NAME]['trails']
-
 	
-"""
-def is_duplicate(kml, collection):
-	""" check whether a record with EXACT same
-		date already exists in given mongo collection
-	"""
+def is_duplicate(md5sum, collection):
+	 '''check whether a record with same md5sum
+		already exists in given mongo collection'''
 	
-	results = [ i for i in collection.find({'date': kml['date']}) ]
+	results = [ i for i in collection.find({'md5sum': md5sum}) ]
 	if results:
 		print('\nDuplicate found! %s\n' % item)
 		return True
 	else:
 		return False
 
-def process(kml_file, kmz=False):
-	""" expects to be passed either a kmz or kml file """
-	try:
-		if kmz:
-			zipped = zipfile.ZipFile(kml_file)
-			kml = Kml(zipped.open('doc.kml'))
-		else: 
-			kml = Kml(open(kml_file))
-	except Exception as e:
-		print('Failed for %s: %s' % (kml_file, e))
-	else:
-		print('FILE NAME: %s' % kml_file)
-		if not is_duplicate(kml.as_dict(), collection): 
-			# try to update database AND
-			# extract files to right place; if one
-			# fails, undo the other:	
-			try:
-				collection.insert_one(kml.as_dict())
-			except Exception as e:
-				print('Failed to update database with %s: %s' % (kml, e))
-			else:
-				try:
-					dest = 'static/kml/%s' % kml.uid
-					if kmz:
-						zipped.extractall(dest)
-					else:
-						if not os.path.exists(os.path.dirname(dest)): os.makedirs(os.path.dirname(dest))
-						shutil.copy(kml_file, '%s/doc.kml' % dest)
-				except Exception as e:
-					print('Failed to extract files: %s\n\tTrying to remove record from database...' % e)
-					try:
-						collection.remove(kml.as_json())
-					except Exception as e:
-						print('Failed to remove item from database -- db is no longer consistent w/ file system: %s' % e)
-	finally:
-		if kmz:
-			zipped.close()
+def process(jpeg_file, collection):
+	''' expects to be passed the path to a jpeg file, and
+		a db collection handle '''
+
+	'''
+	hashlib.md5(open('/home/backup/pictures/2015 vacation/20150907_130923.jpg', 'rb').read()).hexdigest()
+
+	) open, generate md5sum
+	) check if it's duplicate in DB
+	if yes: 
+		if file exists on file system:
+			if md5sum(file) == file name:
+				it's a duplicate -- return True
 		else:
-			kml.close()
+			delete db entry
+			return False
+	if no:
+		write db entry
+		write file (possibly overwriting)
+
+	'''
+	print('FILE NAME: %s' % jpeg_file)
+	md5sum = hashlib.md5(open(jpeg_file, 'rb').read()).hexdigest()
+	if not is_duplicate(md5sum, collection): 
+		# try to update database AND
+		# put files in right place; if one
+		# fails, undo the other:	
+
+
+		#### LEFT OFF HERE
+		try:
+			collection.insert_one(kml.as_dict())
+		except Exception as e:
+			print('Failed to update database with %s: %s' % (jpeg, e))
+		else:
+			try:
+			except Exception as e:
+				try:
+					collection.remove(DICT)
+				except Exception as e:
+					print('Failed to remove item from database -- db is no longer consistent w/ file system: %s' % e)
 		
-"""
 if __name__ == '__main__':
-	""" iterate through images in directory passed and 
-		process appropriately """
+	''' iterate through images in directory passed and 
+		process appropriately '''
+
+	import sys
+	import hashlib
+
+	MONGODB_HOST = 'localhost'
+	MONGODB_PORT = 27017
+	connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+	DB_NAME = 'photo_mapper'
+	collection = connection[DB_NAME]['photo_mapper']
 
 	if len(sys.argv) != 2:
 		print('Usage: %s <directory name>' % sys.argv[0]
@@ -90,6 +88,6 @@ if __name__ == '__main__':
 		sys.exit(2)
 	for item in dir_items:
 		if item[-3:].lower() == 'jpg' or item[-4:].lower() == 'jpeg':
-			process(os.path.join(location,item))
+			process(os.path.join(location,item), collection)
 
 
