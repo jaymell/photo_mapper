55611a7686335c45f48c86112e5cb46148f1a8d1
james <jamesmlucas@gmail.com>
2015-10-19 23:30:44 -0500

    import_data script now working

--- get_jpgps.sh
+++ get_jpgps.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/jaymell/jpgps/master/jpgps.py
+rm jpgps.py* && wget https://raw.githubusercontent.com/jaymell/jpgps/master/jpgps.py
+
--- import_data.py
+++ import_data.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python
 
 import os
-import shutil
 import uuid
-import zipfile
 import datetime
 import dateutil.tz
 import json
 from pymongo import MongoClient
 import re
 
-	
 def is_duplicate(md5sum, collection):
-	 '''check whether a record with same md5sum
+	'''check whether a record with same md5sum
 		already exists in given mongo collection'''
-	
 	results = [ i for i in collection.find({'md5sum': md5sum}) ]
 	if results:
 		print('\nDuplicate found! %s\n' % item)
@@ -22,8 +18,8 @@ def is_duplicate(md5sum, collection):
 	else:
 		return False
 
-def process(jpeg_file, collection):
-	''' expects to be passed the path to a jpeg file, and
+def process(jpeg, collection, dest_folder):
+	''' expects to be passed a jpeg file handle, and
 		a db collection handle '''
 
 	'''
@@ -43,24 +39,32 @@ def process(jpeg_file, collection):
 		write file (possibly overwriting)
 
 	'''
-	print('FILE NAME: %s' % jpeg_file)
-	md5sum = hashlib.md5(open(jpeg_file, 'rb').read()).hexdigest()
+	md5sum = hashlib.md5(jpeg.read()).hexdigest()
 	if not is_duplicate(md5sum, collection): 
 		# try to update database AND
 		# put files in right place; if one
 		# fails, undo the other:	
-
-
-		#### LEFT OFF HERE
 		try:
-			collection.insert_one(kml.as_dict())
+			jpeg_obj = jpgps.Jpgps(jpeg)
 		except Exception as e:
-			print('Failed to update database with %s: %s' % (jpeg, e))
+			print('Failed to instantiate Jpgps object: %s' % e)
+			return
+		the_dict = jpeg_obj.as_dict()
+		the_dict['checksum'] = md5sum
+		try:
+			collection.insert_one(the_dict)
+		except Exception as e:
+			print('Failed to update database with %s: %s' % (jpeg.name, e))
+			return
 		else:
+			# write file:
 			try:
+				with open('%s/%s' % (dest_folder, md5sum), 'wb') as f:
+					jpeg.seek(0)
+					f.write(jpeg.read())
 			except Exception as e:
 				try:
-					collection.remove(DICT)
+					collection.remove(the_dict)
 				except Exception as e:
 					print('Failed to remove item from database -- db is no longer consistent w/ file system: %s' % e)
 		
@@ -68,6 +72,7 @@ if __name__ == '__main__':
 	''' iterate through images in directory passed and 
 		process appropriately '''
 
+	import jpgps
 	import sys
 	import hashlib
 
@@ -76,9 +81,10 @@ if __name__ == '__main__':
 	connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
 	DB_NAME = 'photo_mapper'
 	collection = connection[DB_NAME]['photo_mapper']
+	DEST_FOLDER = './photos'
 
 	if len(sys.argv) != 2:
-		print('Usage: %s <directory name>' % sys.argv[0]
+		print('Usage: %s <directory name>' % sys.argv[0])
 		sys.exit(1)
 	location = sys.argv[1]
 	try:
@@ -88,6 +94,6 @@ if __name__ == '__main__':
 		sys.exit(2)
 	for item in dir_items:
 		if item[-3:].lower() == 'jpg' or item[-4:].lower() == 'jpeg':
-			process(os.path.join(location,item), collection)
-
-
+			with open(os.path.join(location,item), 'rb') as f:
+				print('FILE NAME: %s' % item)
+				process(f, collection, DEST_FOLDER)
