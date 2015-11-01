4d63f3fca2f4134054ef2e53e315419cb069c7db
james <jamesmlucas@gmail.com>
2015-11-01 01:24:41 -0600

    photos now loading when pins clicked on; pins now change color when clicked; map made larger in window

--- public/js/gmaps.js
+++ public/js/gmaps.js
@@ -1,12 +1,25 @@
 var map = (function() {
 
-        var locale = new google.maps.LatLng(30,0);
-        var mapOptions = {
-                center: locale,
-                zoom: 2,
-                mapTypeId: google.maps.MapTypeId.SATELLITE // TERRAIN, SATELLITE, HYBRID, ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
+    var locale = new google.maps.LatLng(30,0);
+    var mapOptions = {
+            center: locale,
+            zoom: 2,
+            mapTypeId: google.maps.MapTypeId.SATELLITE // TERRAIN, SATELLITE, HYBRID, ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
+
+    function setPinColor(pinColor) {
+		return new google.maps.MarkerImage(
+			"http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|"
+			+ pinColor,
+			new google.maps.Size(21, 34),
+			new google.maps.Point(0,0),
+			new google.maps.Point(10, 34)
+    		);
+	};
+	var basePin = setPinColor('FE7569');
+	var changedPin = setPinColor('8169fe');
+
 	return {
 		addPins: function(photoList) {
 			photoList.forEach(function(photo, index, array) {
@@ -14,27 +27,20 @@ var map = (function() {
 					var marker = new google.maps.Marker({
 						position: new google.maps.LatLng(photo.latitude, photo.longitude),
 						title: photo.date,
-					});
-					var CONST = 10;
-					var html = '<div height="' + (photo.height/CONST) + '"' +
-								'width="' + (photo.width/CONST) + '">' +
-								'<img height="' + (photo.height/CONST) + '"' +
-									'width="' + (photo.width/CONST) +'"' +
-									'src="/img/' + 
-									photo.file_name + '">' +
-								'</img></div>';
-                        /*
-						*/
-
-					marker.info = new google.maps.InfoWindow({
-						content: html,
+						map: map,
+						icon: basePin,
 					});
 					marker.addListener('click', function() {
-						// map.setZoom(8);
 						map.setCenter(marker.getPosition());	
-						marker.info.open(map, marker);
+						marker.setIcon(changedPin);	
+						//eventFire(document.getElementById(photo.md5sum), 'click');
+						$.magnificPopup.open({
+							items: {
+								src: '/img/' + photo.file_name,
+							},
+							type: 'image'
+						});
 					});
-					marker.setMap(map);
 				}
 			});	
 		}
@@ -45,14 +51,20 @@ $('#photoList').magnificPopup({
 	delegate: 'a', // child items selector, by clicking on it popup will open
 	type: 'image'
 });
+
 $(document).ready(function() {
 	$.getJSON('/json', function(json) {
 		console.log('got json');
-		map.addPins(json);
 		json.forEach(function(item) {
 			$('#photoList').append(
-				'<li><a href="/img/' + item.file_name + '">' + item.date + '</a></li>' 
+				'<li><a id="' + item.md5sum + '" href="/img/' + item.file_name + '">' + item.date + '</a></li>' 
 			);
 		});
+		map.addPins(json);
+	});
+
+	$('#photoList').magnificPopup({
+    	delegate: 'a', // child items selector, by clicking on it popup will open
+    	type: 'image'
 	});
 });
