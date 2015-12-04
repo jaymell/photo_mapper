323bedebd39254802ee6b6fc0310d983ea554a1d
james <jamesmlucas@gmail.com>
2015-12-04 01:06:47 -0600

    integrated magnific into edit page

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
+
+    $('#photoList').magnificPopup({
+        delegate: 'img', // child items selector, by clicking on it popup will open
+        type: 'image',
+		closeOnContentClick: true,
+	});
+
 	$.getJSON('/json', function(json) {
 		console.log('got json');
 		json.forEach(function(item) {
-			var img=$('<img />').attr('src', '/img/' + item.thumbnail)
+			var img=$('<img/>')
+				.attr('src', '/img/' + item.thumbnail)
+				.attr('class', 'thumnail')
+				.attr('data-mfp-src', '/img/' + item.file_name)
 				.on('load', function() {
 					if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
 						console.log('error loading ' + item.thumbnail);
