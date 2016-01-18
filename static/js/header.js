92a1f6e92be2fb228ab7dbb955489b773b417754
james <jamesmlucas@gmail.com>
2016-01-18 00:15:09 -0600

    album creation, uploads working

--- static/js/header.js
+++ static/js/header.js
@@ -1,5 +1,12 @@
 // submit form when files selected: 
 // http://abandon.ie/notebook/simple-file-uploads-using-jquery-ajax 
+
+function validInput(input) {
+	// allow \w and spaces between \w
+    return input.match(/^[\w]+$/);
+
+}
+
 $('#inFile').on('change', function(e) { 
     files = e.target.files;  
     console.log('files: ', files); 
@@ -11,8 +18,13 @@ $('#inFile').on('change', function(e) {
  
 	if (typeof album == 'undefined') {
 		album = prompt("Enter the album name");
-		// regex testing
+		if ( ! validInput(album) ) {
+			alert('Bad album name: Only letters, numbers, and underscores allowed');
+			location.reload();
+			return;
+		}
 	}
+	console.log('album: ',album);
 	if (user === 'undefined') {
 		alert('user is undefined!');
 		return;
@@ -25,7 +37,7 @@ $('#inFile').on('change', function(e) {
 			cache: false, 
 			processData: false, 
 			contentType: false, 
-			success: function(resp) { console.log('success', resp); }, 
+			success: function(resp) { console.log('success', resp);	}, 
 			failure: function(resp) { console.log('failure', resp); } 
 		}); 
 	}
