72bef2cab66c7fa1f4531f415ecc8acba561ff4c
james <jamesmlucas@gmail.com>
2016-02-13 01:45:02 -0600

    update

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# this needs to read ports and such from config file!!!!!!1
-
-uwsgi -p 2 --threads 10 -s 0.0.0.0:5000 -w app:app 2>&1 --stats 127.0.0.1:5001 | tee -a app.log
+uwsgi -p 2 --threads 10 --http-socket :5000 --wsgi-file app.py --callable app 2>&1 | tee -a app.log
