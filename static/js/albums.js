b516ef317edf3006c0211fd834bbef46c82118dc
james <jamesmlucas@gmail.com>
2016-02-16 00:39:21 -0600

    no more slash addition

--- static/js/albums.js
+++ static/js/albums.js
@@ -21,7 +21,7 @@ function loadData(apiRoute, htmlRoute) {
 					var link = getPhotoLink(photoList);
 					var $img = $("<img></img>")
 						.attr('class', 'thumbnail')
-						.attr('src', photoRoute + '/' + link)
+						.attr('src', photoRoute + link)
 						.attr('height', '100px')
 						.attr('width', '100px');
 					var $a = $('<a></a>')
