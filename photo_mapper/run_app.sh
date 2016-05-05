610426c8ed1a605003e4324957f875452c4e181e
James <jamesmlucas@gmail.com>
2016-05-04 19:09:22 -0500

    use 8 CPUs or 2

--- photo_mapper/run_app.sh
+++ photo_mapper/run_app.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-uwsgi -p 2 --threads 10 -s 0.0.0.0:5000 --wsgi-file app.py --callable app 2>&1 | tee -a app.log
+
+nproc_out=$(nproc)
+num_cpus=${nproc_out-2}
+uwsgi -p $num_cpus --threads 10 -s 0.0.0.0:5000 --wsgi-file app.py --callable app 2>&1 | tee -a app.log
