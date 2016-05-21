d4d07218121b19efbf21f2b7a2e89c10101bb49c
James <jamesmlucas@gmail.com>
2016-05-21 15:40:32 -0500

    initial models built

--- photo_mapper/models.py
+++ photo_mapper/models.py
@@ -1,18 +1,42 @@
 from __future__ import print_function
 import flask
-import flask_sqlalchemy as fsql
+from photo_mapper import db
 
-from photo_mapper import app
-import photo_mapper as pm
-
-db = fsql.SQLAlchemy(app)
-
+# db is coming from main module file:
 class User(db.Model):
-  pk = db.Column(db.Integer, primary_key=True)    
-  user_name = db.Column(db.String(64), unique=True)
+  __tablename__  = 'User'
+  id = db.Column(db.Integer, primary_key=True)    
+  name = db.Column(db.String(64), unique=True)
   
 class Album(db.Model):
+  __tablename__  = 'Album'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(128), unique=True)
+  photos = db.relationship('Photo', secondary='AlbumPhotoLink')
 
 class Photo(db.Model):
+  __tablename__  = 'Photo'
+  id = db.Column(db.Integer, primary_key=True)
+  albums = db.relationship('Album', secondary='AlbumPhotoLink')
+  user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+  md5sum = db.Column(db.String(128), unique=True)
+  date = db.Column(db.DateTime)
+  latitude = db.Column(db.Float)
+  longitude = db.Column(db.Float)
+  sizes = db.relationship('PhotoSizes', back_populates='photo')
+ 
+class AlbumPhotoLink(db.Model):
+  __tablename__  = 'AlbumPhotoLink'
+  album_id = db.Column(db.Integer, db.ForeignKey('Album.id'), primary_key=True)
+  photo_id = db.Column(db.Integer, db.ForeignKey('Photo.id'), primary_key=True)
 
+class PhotoSizes(db.Model):
+  __tablename__  = 'PhotoSizes'
+  id = db.Column(db.Integer, primary_key=True)
+  photo_id = db.Column(db.ForeignKey('Photo.id'))
+  photo = db.relationship('Photo', back_populates='sizes')
+  size = db.Column(db.String(16))
+  width = db.Column(db.Integer)
+  height = db.Column(db.Integer)
+  # name doesn't need to be stored; it's just a function of md5sum and size
 
