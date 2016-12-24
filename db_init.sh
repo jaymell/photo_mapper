f9ab6e9ca5f0ce9c57a1e656402d6402228c7f52
James <jamesmlucas@gmail.com>
2016-12-24 04:11:48 -0600

    update

--- db_init.sh
+++ db_init.sh
@@ -2,7 +2,7 @@
 
 mysql -u root << EOF
 CREATE DATABASE photo_mapper;
-CREATE USER 'pm'@'localhost' IDENTIFIED BY 'ohsh7Shi';
+CREATE USER 'pm'@'localhost' IDENTIFIED BY "$PASSWORD";
 GRANT ALL PRIVILEGES ON photo_mapper.* to 'pm'@'localhost';
 EOF
 
