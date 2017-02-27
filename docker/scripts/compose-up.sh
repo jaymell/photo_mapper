7d18719e2626dbf8411f466a79483e1caad6d9a9
James <jamesmlucas@gmail.com>
2017-02-26 23:27:09 -0600

    updates

--- docker/scripts/compose-up.sh
+++ docker/scripts/compose-up.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
-ENV=$1
 [[ -z $ENV ]] && ENV="local"
 [[ -z $NGINX_TAG ]] && export NGINX_TAG=latest
 [[ -z $PM_TAG ]] && export PM_TAG=latest
 [[ -z $CONFIG_BUCKET ]] && echo "export Bucket name" && exit 1
 
-export CONFIG_BUCKET
-pushd $ENV
+export CONFIG_BUCKET=$CONFIG_BUCKET
 export MYSQL_HOST=$(ip addr | grep docker0 | grep inet  | awk '{print $2}' | awk -F/ '{print $1}')
+pushd $ENV
 docker-compose up -d --force-recreate
