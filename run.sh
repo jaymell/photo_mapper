e7dc4ddac12eee0bd0a608f526a160495c0600b4
James <jamesmlucas@gmail.com>
2017-02-26 22:50:07 -0600

    add example systemd unit file

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-aws s3 cp s3://$BUCKET/$ENV/config ./
+aws s3 cp s3://$CONFIG_BUCKET/$ENV/config ./
 nproc_out=$(nproc)
 num_cpus=${nproc_out-2}
 #uwsgi -p $num_cpus --threads 10 -s 0.0.0.0:5000 --module photo_mapper --callable app 2>&1 | tee -a app.log
